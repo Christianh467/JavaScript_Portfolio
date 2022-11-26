5afe1086bd05d2df1b5c6ddd2f6d9875d0de6220
Christian Head <106939542+Christianh467@users.noreply.github.com>
2022-11-25 19:54:38 -0600

    Update NoPairValue.js

--- NoPairValue.js
+++ NoPairValue.js
@@ -13,3 +13,16 @@ function solution(A) {
     }
     return parseInt(A);
 }
+
+//OR
+
+function check(array) {
+    var s = new Set;                                //sets are objects with unique keys/values, can't duplicate
+    
+    array.forEach(v => s.delete(v) || s.add(v));    //delete the value from set if present, if not add value to set
+
+    const myArray = Array.from(s);                  //put objects values into array if more than one value still present in iterator set
+    
+    return s.values().next().value;                 //values() shows values in iterator set so far {5, 7}
+}                                                   //.next() selects the next object {value: 5, done: false}
+                                                    //.value return this iterator objects value property
