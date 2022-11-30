846e3e2f88dc198ccfe9273e48be4ef6dcf32ad6
Christian Head <106939542+Christianh467@users.noreply.github.com>
2022-11-30 17:44:56 -0600

    Update NoPairValue.js

--- NoPairValue.js
+++ NoPairValue.js
@@ -1,21 +1,3 @@
-function solution(A) {
-    for(let i = 0; i < A.length; i){
-        let single = A[i];
-        let pairIndex = A.slice(i + 1).findIndex(value => value === single)
-
-        if(pairIndex >= 0){
-            A.splice(pairIndex + i + 1, 1);
-            A.splice(i, 1);
-        }
-        else{
-            i++
-        }
-    }
-    return parseInt(A);
-}
-
-//OR
-
 function check(array) {
     var s = new Set;                                //sets are objects with unique keys/values, can't duplicate
     
